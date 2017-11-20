df3af88ca02c19664b04dcef42b51bff8432876e
Quentin <upafourth@gmail.com>
2017-11-20 10:17:11 +0100

    new class4

---
+++ testconsole/Class4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testconsole
+{
+    class Class4
+    {
+        //this is a new class created on VS
+    }
+}
